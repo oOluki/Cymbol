757af9aa789a599937333448ceffa342f68fbac2
Lucas Ferreira <dummyconta1234@gmail.com>
2024-12-03 01:10:04 -0300

    (Console Mode) Simple Add And Delete Contexts

--- src/main.c
+++ src/main.c
@@ -1,14 +1,181 @@
 #include "cymbol.h"
 #include "cymath.h"
+#include "parser.h"
+
+#if defined(__linux__) || defined(APPLE)
+    #define CLEAR_VIEW system("clear");
+#elif defined(_WIN32)
+    #define CLEAR_VIEW system("cls");
+#else
+    #define CLEAR_VIEW printf("\n\n\n\n\n\n\n\n\n\n\n");
+#endif
+
+CymContext* contexts = NULL;
+
+struct Config{
+    int context_count;
+    int current_context;
+    int capacity;
+} config = {1, 0, 1};
+
+int digit(char c){
+    switch (c){
+    case '0': return 0;
+    case '1': return 1;
+    case '2': return 2;
+    case '3': return 3;
+    case '4': return 4;
+    case '5': return 5;
+    case '6': return 6;
+    case '7': return 7;
+    case '8': return 8;
+    case '9': return 9;
+    default : return -1;
+    }
+}
+
+int get_literal(const char* str){
+
+    int i = 0;
+    int _10expn = 1;
+    size_t size = 0;
+
+    for(; str[size]; size+=1);
+
+    for(int n = size - 1; n > -1; n-=1){
+
+        const int d = digit(str[n]);
+
+        if(d < 0){
+            fprintf(
+                stderr,
+                "[ERROR] Expected Int Literal, Got '%s' Instead. "
+                "Character '%c' Is Not A Digit\n",
+                str, str[n]
+            );
+            return -1;
+        }
+
+        i += d * _10expn;
+        _10expn *= 10;
+
+    }
+
+    return i;    
+}
+
+// returns -1 to request exit, an error code or 0 on success
+int perform_command(const UserPrompt prompt){
+
+    int reg = 0;
+
+    switch (prompt.type)
+    {
+    case USRPROMPT_EXIT: return -1;
+    case USRPROMPT_CLEARVIEW: CLEAR_VIEW; break;
+    case USRPROMPT_CLEARMEM:
+        for(int i = 0; i < config.context_count; i+=1){
+            cym_destroy_context(contexts + i);
+        }
+        config.context_count = 1;
+        config.current_context = 0;
+        config.capacity = 1;
+        free(contexts);
+        contexts = (CymContext*)malloc(sizeof(CymContext));
+        break;
+    case USRPROMPT_ADDCONTEXT:
+        if(config.context_count + 1 > config.capacity){
+            CymContext* ncontexts = (CymContext*)malloc((config.context_count + 1) * sizeof(CymContext));
+            memcpy(ncontexts, contexts, config.context_count * sizeof(CymContext));
+            free(contexts);
+            contexts = ncontexts;
+            config.capacity += 1;
+        }
+        contexts[config.context_count] = cym_create_context(10);
+        config.current_context = config.context_count;
+        config.context_count += 1;
+        break;
+    case USRPROMPT_DELCONTEXT:
+        cym_destroy_context(contexts + config.current_context);
+        for(size_t i = config.current_context; i < config.context_count - 1; i+=1){
+            contexts[i] = contexts[i + 1];
+        }
+        if(config.context_count > 1) config.context_count -= 1;
+        if(config.current_context && config.current_context == config.context_count){
+            config.current_context -= 1;
+        }
+        break;
+    case USRPROMPT_CHANGECONTEXT:
+        reg = get_literal(prompt.operand1.as_str);
+        if(reg < 1 || reg > config.context_count){
+            fprintf(stderr, "[ERROR] Can't Change To Non Existing Context '%i'\n", reg);
+            return 3;
+        }
+        config.current_context = reg - 1;
+        break;
+    
+    default:
+        CLEAR_VIEW;
+        return 1;
+    }
+
+    return 0;
+}
 
 
 int main(int argc, char** argv){
 
+    Mc_token_t* token_buffer = mc_create_token_buffer(100, 1000);
+    
+    contexts = (CymContext*)malloc(sizeof(CymContext));
 
+    if(argc < 2){
 
+        
+        char buffer[1000];
 
+        do{            
+            printf(
+                "\tcurrent context: %i of %i (capacity=%i)\n\n(Cymbol) ",
+                config.current_context + 1, config.context_count, config.capacity
+            );
 
-    return 0;
+            memset(buffer, 0, 1000);
+            fgets(buffer, 999, stdin);
+
+            size_t token_count = mc_tokenize(&token_buffer, (unsigned int)0, buffer, NULL, "", "");
+
+            const UserPrompt prompt = parse(token_count, token_buffer);
+
+            const int response = perform_command(prompt);
+
+            if(response < 0) break;
+
+        } while(!feof(stdin));
+
+        printf("\n");
+
+        mc_destroy_token_buffer(token_buffer);
+
+        return 0;
+    }
+
+    size_t token_count = 0;
+
+    for(int i = 1; i < argc; i+=1){
+
+        token_count += mc_tokenize(
+            &token_buffer, token_count,
+            argv[i], "", "-", ""
+        );
+
+    }
+
+    UserPrompt prompt = parse(token_count, token_buffer);
+
+    mc_destroy_token_buffer(token_buffer);
+
+    return perform_command(prompt) ^ (int)(-1);
 }
 
 
--- src/parser.h
+++ src/parser.h
@@ -7,12 +7,14 @@
 typedef enum UserPromptType
 {
 
-    USERPROMPT_NONE = 0,
+    USRPROMPT_NONE = 0,
 
-    USERPROMPT_CLEARVIEW,
-    USERPROMPT_CLEARMEM,
+    USRPROMPT_EXIT,
+    USRPROMPT_CLEARVIEW,
+    USRPROMPT_CLEARMEM,
 
-    USRPROMPT_CLEARCONTEXT,
+    USRPROMPT_ADDCONTEXT,
+    USRPROMPT_DELCONTEXT,
     USRPROMPT_CHANGECONTEXT,
     USRPROMPT_LOADCONTEXT,
     USRPROMPT_SAVECONTEXT,
@@ -24,23 +26,31 @@ typedef enum UserPromptType
 
 } UserPromptType;
 
+typedef struct UserPromptInst
+{
+    UserPromptType type;
+    uint8_t expected_operands;
+} UserPromptInst;
+
+typedef union {
+    size_t      as_index;
+    void*       as_ptr;
+    const char* as_str;
+} Operand;
 
 typedef struct UserPrompt
 {
     UserPromptType type;
-    union Operand
-    {
-        size_t      as_index;
-        void*       as_ptr;
-        const char* as_str
-    } operand1, operand2, operand3;
-
+    Operand operand1;
+    Operand operand2;
+    Operand operand3;
     uint64_t flags;
     
 } UserPrompt;
 
 
 uint64_t get_flag(char c){
+
     switch (c)
     {
     case 'c':
@@ -56,12 +66,31 @@ uint64_t get_flag(char c){
 }
 
 
+UserPromptInst get_prompt_inst(const char* prompt_inst){
+
+    if(mc_compare_str(prompt_inst, "exit", 0))  return (UserPromptInst){USRPROMPT_EXIT, 0};
+    if(mc_compare_str(prompt_inst, "cv", 0))    return (UserPromptInst){USRPROMPT_CLEARVIEW, 0};
+    if(mc_compare_str(prompt_inst, "cm", 0))    return (UserPromptInst){USRPROMPT_CLEARMEM, 0};
+    if(mc_compare_str(prompt_inst, "nc", 0))    return (UserPromptInst){USRPROMPT_ADDCONTEXT, 0};
+    if(mc_compare_str(prompt_inst, "delc", 0))  return (UserPromptInst){USRPROMPT_DELCONTEXT, 0};
+    if(mc_compare_str(prompt_inst, "chc", 0))   return (UserPromptInst){USRPROMPT_CHANGECONTEXT, 1};
+    if(mc_compare_str(prompt_inst, "load", 0))  return (UserPromptInst){USRPROMPT_LOADCONTEXT, 1};
+    if(mc_compare_str(prompt_inst, "save", 0))  return (UserPromptInst){USRPROMPT_SAVECONTEXT, 0};
+    if(mc_compare_str(prompt_inst, "merge", 0)) return (UserPromptInst){USRPROMPT_MERGECONTEXTS, 1};
+    if(mc_compare_str(prompt_inst, "new", 0))   return (UserPromptInst){USRPROMPT_ADDCYM, 3};
+    if(mc_compare_str(prompt_inst, "del", 0))   return (UserPromptInst){USRPROMPT_DELCYM, 1};
+    if(mc_compare_str(prompt_inst, "set", 0))   return (UserPromptInst){USRPROMPT_SETCYM, 2};
+    if(mc_compare_str(prompt_inst, "print", 0)) return (UserPromptInst){USRPROMPT_SHOWCYM, 1};
+
+    return (UserPromptInst){USRPROMPT_NONE, 0};
+}
+
 UserPrompt parse(size_t token_count, const Mc_token_t* token_buffer){
 
     UserPrompt output;
 
     output.flags = 0;
-    output.type = USERPROMPT_NONE;
+    output.type = USRPROMPT_NONE;
     output.operand1.as_index = 0;
     output.operand2.as_index = 0;
     output.operand3.as_index = 0;
@@ -72,18 +101,18 @@ UserPrompt parse(size_t token_count, const Mc_token_t* token_buffer){
     for(size_t i = 0; i < token_count; i+=1){
         const char* token_str = mc_get_token_data(token_buffer, token_buffer[i]);
 
-        if(mc_compare_str(token_str, "-", 1)){
-            if(i + 1 > token_count){
+        if(token_str[0] == '-'){
+            if(!token_str[1]){
                 fprintf(stderr, "[ERROR] Expected Flag Identifier After '-'\n");
                 err = 1;
                 continue;
-            } else if(mc_str_size(token_str) != 1){
+            } else if(token_str[2]){ // flags should only have two characters, the standart '-' and the identifier
                 fprintf(stderr, "[ERROR] Invalid Flag Identifier '%s'\n", token_str);
                 err = 1;
                 continue;
             }
 
-            const uint64_t flag = get_flag(token_str[0]);
+            const uint64_t flag = get_flag(token_str[1]);
 
             if(!flag) err = 1;
 
@@ -92,10 +121,33 @@ UserPrompt parse(size_t token_count, const Mc_token_t* token_buffer){
         }
     }
 
+    for(size_t i = 0; i < token_count; i+=1){
+        const char* token_str = mc_get_token_data(token_buffer, token_buffer[i]);
+
+        if(token_str[0] != '-'){ // token is not a flag
+            UserPromptInst inst = get_prompt_inst(token_str);
+
+            if(inst.expected_operands != token_count - i - 1){
+                fprintf(
+                    stderr, "[ERROR] Instruction %s Expects %u Operands, Got %zu Instead\n",
+                    token_str, inst.expected_operands, token_count - i - 1
+                );
+                err = 1;
+                return output;
+            }
+
+            output.type = inst.type;
+            output.operand1.as_str = (inst.expected_operands > 0)? mc_get_token_data(token_buffer, token_buffer[i + 1]) : NULL;
+            output.operand2.as_str = (inst.expected_operands > 1)? mc_get_token_data(token_buffer, token_buffer[i + 2]) : NULL;
+            output.operand3.as_str = (inst.expected_operands > 2)? mc_get_token_data(token_buffer, token_buffer[i + 3]) : NULL;
+
+            return output;
+        }
+
+    }
+
 
     return output;
-
-
 }
 
 
